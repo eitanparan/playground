317e0e4539cccfbc4b8ecd166ee3a90553f766b9
eitanparan <44940517+eitanparan@users.noreply.github.com>
2024-01-30 03:09:10 +0200

    Update groovy_script.groovy

--- groovy_script.groovy
+++ groovy_script.groovy
@@ -8,6 +8,9 @@ pipeline {
                 script {
                     def rootDir = pwd()
                     echo "${rootDir}"
+                    sh 'ls'
+                    sh 'ls -l'
+                    sh 'pwd'
                     /*def greet = load "g_script.groovy"
                     greet.greet()*/
                 }

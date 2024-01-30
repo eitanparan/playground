4fa2d3f892876c58b3cc359ff8c9fb019d18e91e
eitanparan <44940517+eitanparan@users.noreply.github.com>
2024-01-30 02:55:22 +0200

    Update groovy_script.groovy

--- groovy_script.groovy
+++ groovy_script.groovy
@@ -1,10 +1,16 @@
+def rootDir = pwd()
+def greet = load "${rootDir}@script/g_script.groovy"
+
 pipeline {
     agent any
-
+    
     stages {
         stage('Hello') {
             steps {
                 echo 'Hello World Eitan'
+                scrips {
+                    greet.greet()
+                }
             }
         }
     }
